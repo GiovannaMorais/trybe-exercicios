6ffc960966e3b5318f04067c249a27665e93ff7f
GiovannaMorais <gigimorais9@hotmail.com>
2022-03-14 08:25:20 -0300

    Exercicios de seletores

---
+++ Fundamentos/Bloco 5/JavaScript/DOM e Seletores/Seletores de elementos/scripts.js
@@ -0,0 +1,7 @@
+console.log(document.getElementById ("page-title"));
+document.getElementById("page-title").innerText = "A Espera de um Milagre"
+console.log(document.getElementById ("paragraph"));
+document.getElementById("paragraph").innerText = "Saber esperar é uma virtude! Aceitar, sem questionar, que cada coisa tem um tempo certo para acontecer... é ter Fé!"
+console.log(document.getElementById ("subtitle"));
+document.getElementById("subtitle").innerText = "Sinopse"
+document.getElementById("second-paragraph").innerText = "Um carcereiro tem um relacionamento incomum com um preso que se encontra no corredor na morte: Coffey, condenado por ter matado brutalmente duas gêmeas de nove anos. Ele tem tamanho e força para ter cometido o crime, mas seu comportamento é completamente oposto à sua aparência. Além de ser simples, ingênuo e ter pavor do escuro, possui um dom sobrenatural.";
---
+++ Fundamentos/Bloco 5/JavaScript/DOM e Seletores/Seletores de elementos/scripts2.js
@@ -0,0 +1,4 @@
+console.log(document.getElementsByClassName ("paragrafos"));
+document.getElementsByClassName("paragrafos")[0].style.color = "blue";
+console.log(document.getElementsByTagName("h4"));
+document.getElementsByTagName("h4")[0].style.color= "purple";
