108517503195646be6450ce1183ce38cc88a9d74
GiovannaMorais <gigimorais9@hotmail.com>
2022-03-24 12:33:28 -0300

    Exericicos 6.1

---
+++ Fundamentos/Bloco 6/HTML e CSS: Forms, Flexbox e Responsivo/HTML & CSS - Forms/Criando Formulário/script.js
@@ -0,0 +1,21 @@
+const button = document.getElementById('submit')
+function reverseButton(event){
+    event.preventDefault();   
+}
+button.addEventListener('click',reverseButton);
+
+function clearButton(){
+const tudo = document.querySelectorAll('input')
+const textArea =document.querySelector('textarea');
+for (let index = 0; index < tudo.length; index += 1) {
+   const posicao= tudo[index];
+    posicao.value='';
+    posicao.checked = false;
+}
+textArea='';
+}
+window.onload=function(){
+    const clear = document.getElementById('limpar');
+    clear.addEventListener('clik',clearButton)
+    button.addEventListener('click',reverseButton)
+}
