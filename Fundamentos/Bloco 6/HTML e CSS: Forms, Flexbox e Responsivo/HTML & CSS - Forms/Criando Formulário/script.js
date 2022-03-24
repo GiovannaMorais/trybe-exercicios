f2758bd934049eebaae51a318de73305e722a91f
GiovannaMorais <gigimorais9@hotmail.com>
2022-03-24 12:37:10 -0300

    Exercicios 6.1

--- Fundamentos/Bloco 6/HTML e CSS: Forms, Flexbox e Responsivo/HTML & CSS - Forms/Criando Formulário/script.js
+++ Fundamentos/Bloco 6/HTML e CSS: Forms, Flexbox e Responsivo/HTML & CSS - Forms/Criando Formulário/script.js
@@ -1,21 +1,23 @@
-const button = document.getElementById('submit')
-function reverseButton(event){
-    event.preventDefault();   
+const button = document.getElementById("submit");
+function reverseButton(event) {
+  event.preventDefault();
 }
-button.addEventListener('click',reverseButton);
+window.onload = function () {
+  button.addEventListener("click", reverseButton);
+};
 
-function clearButton(){
-const tudo = document.querySelectorAll('input')
-const textArea =document.querySelector('textarea');
-for (let index = 0; index < tudo.length; index += 1) {
-   const posicao= tudo[index];
-    posicao.value='';
+function clearButton() {
+  const tudo = document.querySelectorAll("input");
+  const textArea = document.querySelector("textarea");
+  for (let index = 0; index < tudo.length; index += 1) {
+    const posicao = tudo[index];
+    posicao.value = "";
     posicao.checked = false;
+  }
+  textArea = "";
 }
-textArea='';
-}
-window.onload=function(){
-    const clear = document.getElementById('limpar');
-    clear.addEventListener('clik',clearButton)
-    button.addEventListener('click',reverseButton)
-}
+window.onload = function () {
+  const clear = document.getElementById("limpar");
+  clear.addEventListener("clik", clearButton);
+  button.addEventListener("click", reverseButton);
+};
