7d007b457a99f41026fb0a1ee80540dd193434b9
GiovannaMorais <gigimorais9@hotmail.com>
2022-04-07 10:12:24 -0300

    Exercicios Bonus 8.1

--- Fundamentos/Bloco 8/JavaScript ES6 - Introdução a Higher Order Functions/Exercicios 8.1/bonus.js
+++ Fundamentos/Bloco 8/JavaScript ES6 - Introdução a Higher Order Functions/Exercicios 8.1/bonus.js
@@ -22,7 +22,7 @@ const mage = {
 
 
   const danoDragon = (dragon)=>{
-      const number = Math.floor((Math.random() * (dragon.strength - 15 + 1)));
+      const number = Math.floor((Math.random() * (dragon.strength - 15 + 1)+ 15));
       return number
   }
 
@@ -32,16 +32,16 @@ const mage = {
     return number2
 }
 
-const obj=(mage)=>{
+const danoMage=(mage)=>{
     const mana = mage.mana
     const turnStats = {
         manaGasta: 0,
         mensagem: "Não possui mana suficiente"
     }
 if(mana > 15){
-    const number3 = Math.floor((Math.random() *  (mage.intelligence * 2 - mage.intelligence +1 )) + mage.intelligence);
+    const mageDamage = Math.floor((Math.random() *  (mage.intelligence * 2 - mage.intelligence +1 )) + mage.intelligence);
     turnStats.manaGasta =15;
-    turnStats.mensagem = number3;
+    turnStats.mensagem = mageDamage;
     return turnStats;
 
 }
@@ -49,4 +49,29 @@ return turnStats;
 
 }
 
-
+const gameActions = {
+    warriorTurn: (danoWarrior) => {
+      const warriorDamage = danoWarrior(warrior);
+      warrior.damage = warriorDamage;
+      dragon.healthPoints -= warriorDamage;
+    },
+    mageTurn: (danoMage) => {
+      const mageTurnStats = danoMage(mage);
+      const mageDamage = mageTurnStats.mensagem;
+      mage.damage = mageDamage;
+      mage.mana -= mageTurnStats.manaGasta;
+      dragon.healthPoints -= mageDamage;
+    },
+    dragonTurn: (danoDragon) => {
+      const dragonDamage = danoDragon(dragon);
+      mage.healthPoints -= dragonDamage;
+      warrior.healthPoints -= dragonDamage;
+      dragon.damage = dragonDamage;
+    },
+    turnResults: () => battleMembers,
+  };
+  
+  gameActions.warriorTurn(danoWarrior);
+  gameActions.mageTurn(danoMage);
+  gameActions.dragonTurn(danoDragon);
+  console.log(gameActions.turnResults());
