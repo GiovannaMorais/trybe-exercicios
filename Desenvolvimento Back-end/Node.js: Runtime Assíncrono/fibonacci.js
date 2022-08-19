e0977580801f1100b56813d003319aa1719496a6
GiovannaMorais <gigimorais9@hotmail.com>
2022-08-19 12:00:58 -0300

    Exercicios 22.1

--- Desenvolvimento Back-end/Node.js: Runtime Assíncrono/fibonacci.js
+++ Desenvolvimento Back-end/Node.js: Runtime Assíncrono/fibonacci.js
@@ -1,21 +1,13 @@
 const readline = require('readline-sync');
 
 function handleFibonacci(quantity) {
-  // a armazena o último número que calculamos
   let a = 1;
-  // b armazena o penúltimo número que calculamos
   let b = 1;
 
-  // Repetimos o loop enquanto `n` for maior que 0
   for (let n = quantity; n >= 0; n -= 1) {
     if (b) console.log(b);
-    // Armazenamos o último valor calculado em uma variável temporária
     const temp = a;
-    // Para calcular o novo valor, somamos o último valor com o penúltimo valor
-    // O novo valor é armazenado em `a`, já que ele passa a ser o último valor calculado
     a += b;
-    // O valor antigo de `a`, que estava armazenado na variável temporária
-    // agora se torna o penúltimo número e, por isso, é armazenado em `b`
     b = temp;
   }
 
